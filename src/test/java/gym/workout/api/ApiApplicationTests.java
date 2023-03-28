32a1eb56e293a839c14d79371190f9b408faec25
Allas Assis <allasassis10@gmail.com>
2023-03-28 16:05:51 -0300

    Projeto criado

---
+++ src/test/java/gym/workout/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gym.workout.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
